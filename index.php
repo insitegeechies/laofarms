1c2475b6a82e8cd86985173e7d4c3c295fd70651
Renniel <insitegeechies@gmail.com>
2024-01-28 18:01:55 -0500

    Signed-off-by: Renniel <insitegeechies@gmail.com>

---
+++ index.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang="en" data-theme="fantasy">
+<head>
+    <meta charset="utf-8" />
+    <meta content='text/html; charset=utf-8' http-equiv='Content-Type'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content="width=device-width,height=device-height, initial-scale=1, shrink-to-fit=yes">
+
+    <div itemscope itemtype="https://schema.org/CreativeWork">
+        <meta itemprop="name" content="" />
+        <link itemprop="url" href="" />
+        <meta itemprop="description" content="" />
+        <div itemprop="author" itemscope itemtype="https://schema.org/Person">
+            <meta itemprop="name" content="" />
+        </div>
+        <meta itemprop="datePublished" content="2022-05-23" />
+        <meta itemprop="keywords" content="" />
+        <div itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+            <meta itemprop="name" content=""/>
+            <link itemprop="url" href="">
+        </div>
+        <link itemprop="image" href="">
+        <meta itemprop="inLanguage" content="en">
+    </div>
+
+    <!--theme colors -->
+    <meta name="theme-color" content="" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+    <!--Basic meta info -->
+    <meta name="keywords" content="">
+    <meta name="author" content="" />
+    <meta name="description" content="">
+
+    <!--OpenGraph meta -->
+    <meta property="og:description" content=""/>
+    <meta property="og:title" content="" />
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content="" />
+    <meta property="og:type" content="" />
+
+    <!--meta for twitter -->
+    <meta name="twitter:card" content="">
+    <meta name="twitter:creator" content=">">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:image" content="">
+    <meta name="twitter:site" content="">
+    <meta name="twitter:description" content=""">
+
+    <meta name="robots" content="index, follow">
+
+    <link rel="dns-prefetch" href="//wrensitefx.com">
+
+    <!--Favicon-->
+    <link rel="icon" type="image/webp" href="assets/images/icons/logo/favicon-192x192.webp" sizes="192x192"/>
+    <link rel="icon" type="image/webp" href="assets/images/icons/logo/favicon-160x160.webp" sizes="160x160"/>
+    <link rel="icon" type="image/webp" href="assets/images/icons/logo/favicon-96x96.webp" sizes="96x96"/>
+    <link rel="icon" type="image/webp" href="assets/images/icons/logo/favicon-32x32.webp" sizes="32x32"/>
+    <link rel="icon" type="image/webp" href="assets/images/icons/logo/favicon-16x16.webp" sizes="16x16"/>
+    <link rel="apple-touch-icon" type="image/webp" href="assets/images/icons/logo/apple-touch-icon-180x180.webp" sizes="180x180"/>
+    <link rel="apple-touch-icon" type="image/webp" href="assets/images/icons/logo/apple-touch-icon-152x152.webp" sizes="152x152"/>
+    <link rel="apple-touch-icon" type="image/webp" href="assets/images/icons/logo/apple-touch-icon-144x144.webp" sizes="144x144"/>
+    <link rel="apple-touch-icon" type="image/webp" href="assets/images/icons/logo/apple-touch-icon-120x120.webp" sizes="120x120"/>
+    <link rel="apple-touch-icon" type="image/webp" href="assets/images/icons/logo/apple-touch-icon-114x114.webp" sizes="114x114"/>
+    <link rel="apple-touch-icon" type="image/webp" href="assets/images/icons/logo/apple-touch-icon-76x76.webp" sizes="76x76"/>
+    <link rel="apple-touch-icon" type="image/webp" href="assets/images/icons/logo/apple-touch-icon-72x72.webp" sizes="72x72"/>
+    <link rel="apple-touch-icon" type="image/webp" href="assets/images/icons/logo/apple-touch-icon-60x60.webp" sizes="60x60"/>
+    <link rel="apple-touch-icon" type="image/webp" href="assets/images/icons/logo/apple-touch-icon-52x52.webp" sizes="52x52"/>
+    <link rel="icon" href="assets/images/icons/logo/favicon.ico?v=2" type="image/x-icon">
+
+    <title></title>
+
+    <!-- stylesheets -->
+    <link rel="preload" href="assets/css/styles.css" as="style">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <script rel="preload" src="assets/js/darkModeToggle.min.js"></script>
+
+<body class="antialiased overflow-x-hidden">
+
+<main>
+    <div class="min-h-32 flex justify-center items-center fixed w-full z-50 bg-amber-500">
+        <div class="navbar container mx-auto">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-auto">
+                        <li><a>Item 1</a></li>
+                        <li>
+                            <a>Parent</a>
+                            <ul class="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div>
+                <a class="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal px-1">
+                    <li><a>Item 1</a></li>
+                    <li>
+                        <details>
+                            <summary>Parent</summary>
+                            <ul class="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Item 3</a></li>
+                </ul>
+            </div>
+            <div class="navbar-end">
+                <a class="btn">Button</a>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="hero min-h-screen" style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+            <div class="hero-overlay bg-opacity-60"></div>
+            <div class="hero-content text-center text-neutral-content">
+                <div class="max-w-md">
+                    <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+                    <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous" rel="preload"></script>
+<script src="https://kit.fontawesome.com/fe6c44cf74.js" crossorigin="anonymous" rel="preload"></script>
+<script defer src="https://unpkg.com/alpinejs@3.10.5/dist/cdn.min.js" rel="preload"></script>
+<script src="https://unpkg.com/aos@next/dist/aos.js" rel="preload"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js" rel="preload"></script>
+<script src="assets/js/darkMode.min.js" rel="preload"></script>
+<script src="assets/js/smoothscroll.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/detect-browser@5.3.0/index.min.js" rel="preload"></script>
+</body>
+</html>
